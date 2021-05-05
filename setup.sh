2b9bbb8e42ecf19769e8b60995ce1ece4ae492bd
Swapnil khairnar <64456709+imswapnil99@users.noreply.github.com>
2021-05-05 12:06:10 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,12 @@ echo "I am $remark too!"
 
 echo "Please be patient ->> INSTALLATION WILL TAKE SOME TIME ;)"
 
-sudo su - root
+sudo yum upgrade -y
 yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade -y
+
 sudo yum install jenkins java-1.8.0-openjdk-devel -y
 sudo systemctl daemon-reload
 sudo systemctl status jenkins
