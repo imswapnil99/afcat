4d193f753603232dd98d65c4d3e60965f3d37d7d
Swapnil khairnar <64456709+imswapnil99@users.noreply.github.com>
2021-05-06 11:27:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ echo "I am $remark too!"
 
 echo "Please be patient ->> INSTALLATION WILL TAKE SOME TIME ;)"
 
-sudo yum upgrade -y
+#sudo yum upgrade -y
 yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
